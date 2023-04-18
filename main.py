b7e79d093d6d27ee9143f09ce8073a5203335c79
Robert Lockay <robert.lockay@gmail.com>
2023-04-18 17:24:44 -0500

    added python SQL connector

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
 print("Hello World")
+import mysql.connector
+
+
+
+connection = mysql.connector.connect(user = 'root', database = 'example', password = '!098c2cPa$$word')
+
+
+
+cursor = connection.cursor()
+
+
+
+testQuery = ("SELECT * FROM table_name")
+
+
+
+cursor.execute(testQuery)
+
+
+
+for item in cursor:
+    print(item)
+
+
+
+cursor.close()
+
+connection.close()
